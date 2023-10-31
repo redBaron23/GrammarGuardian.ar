07a8f776038e9b44e3ae398b8bb387fab2f50afd
Pato <23110463+redBaron23@users.noreply.github.com>
2023-10-31 01:18:45 -0300

    add icon

--- components/atoms/SendButton.tsx
+++ components/atoms/SendButton.tsx
@@ -1,17 +1,22 @@
-import Image from "next/image";
-
 const SendButton = () => {
   return (
     <button className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
       <span>Send</span>
       <span className="ml-2">
-        <Image
-          src="/send.svg"
-          alt="Send Icon"
-          className="dark:invert"
-          priority
-          fill
-        />
+        <svg
+          className="w-4 h-4 transform rotate-45 -mt-px"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+          ></path>
+        </svg>
       </span>
     </button>
   );

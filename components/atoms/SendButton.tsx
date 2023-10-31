c23a300a4c1ec9482d1f19be695344d762c26cc3
Pato <23110463+redBaron23@users.noreply.github.com>
2023-10-31 00:58:23 -0300

    finish atomic input styles

--- components/atoms/SendButton.tsx
+++ components/atoms/SendButton.tsx
@@ -1,11 +1,17 @@
-import { ReactComponent as SendIcon } from "../../public/icons/send.svg";
+import Image from "next/image";
 
 const SendButton = () => {
   return (
     <button className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
       <span>Send</span>
       <span className="ml-2">
-        <SendIcon />
+        <Image
+          src="/send.svg"
+          alt="Send Icon"
+          className="dark:invert"
+          priority
+          fill
+        />
       </span>
     </button>
   );
--- components/moleculas/ChatInput.tsx
+++ components/moleculas/ChatInput.tsx
@@ -1,18 +1,21 @@
+import AttachmentInput from "../atoms/AttachmentInput";
 import SendButton from "../atoms/SendButton";
 
 const ChatInput = () => {
   return (
     <div className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
+      <AttachmentInput />
       <div className="flex-grow ml-4">
         <div className="relative w-full">
           <input
             type="text"
             className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
           />
-          <SendButton />
         </div>
       </div>
-      <div className="ml-4"></div>
+      <div className="ml-4">
+        <SendButton />
+      </div>
     </div>
   );
 };

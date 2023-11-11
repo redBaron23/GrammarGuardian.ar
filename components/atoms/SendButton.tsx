ed597428ea661db831c8a52dc01740137f76053e
Pato <23110463+redBaron23@users.noreply.github.com>
2023-11-11 17:07:21 -0300

    add form

--- components/atoms/SendButton.tsx
+++ components/atoms/SendButton.tsx
@@ -1,6 +1,9 @@
 const SendButton = () => {
   return (
-    <button className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+    <button
+      type="submit"
+      className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+    >
       <span>Send</span>
       <span className="ml-2">
         <svg

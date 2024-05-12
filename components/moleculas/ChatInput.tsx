f0f618bade02edede11915368d74d3cf646d431d
Pato <23110463+redBaron23@users.noreply.github.com>
2024-05-12 13:37:16 -0300

    resolve packags issues

--- components/moleculas/ChatInput.tsx
+++ components/moleculas/ChatInput.tsx
@@ -1,18 +1,24 @@
 "use client";
 
-import { useState } from "react";
+import { useActionState } from "react";
 import AttachmentInput from "../atoms/AttachmentInput";
 import SendButton from "../atoms/SendButton";
 
+const initialState = {
+  text: "",
+};
+
 interface Props {
-  action: any;
+  sendMessage: <State>(state: Awaited<State>) => State | Promise<State>;
 }
 
-const ChatInput = ({ action }: Props) => {
+const ChatInput = ({ sendMessage }: Props) => {
+  const [state, formAction] = useActionState(sendMessage, initialState);
+
   return (
     <form
       className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4"
-      action={action}
+      action={formAction}
     >
       <AttachmentInput />
       <div className="flex-grow ml-4">

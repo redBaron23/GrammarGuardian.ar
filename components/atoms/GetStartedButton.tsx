9bc200b7edd97cc112c0f3f9f29362610051f3c9
Pato <23110463+redBaron23@users.noreply.github.com>
2024-06-08 21:49:33 -0300

    styles

--- components/atoms/GetStartedButton.tsx
+++ components/atoms/GetStartedButton.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 const GetStarted = () => {
   return (
     <button
-      onClick={() => signIn()}
+      onClick={() => signIn("google")}
       className="inline-block bg-white text-blue-600 font-semibold py-4 px-8 rounded-full shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
     >
       Get Started

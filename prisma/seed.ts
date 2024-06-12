0fd18a1624e3c95a5763dd585f8a32a6e3ec9cf7
Pato <23110463+redBaron23@users.noreply.github.com>
2024-06-11 21:25:52 -0300

    remove api

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,18 +3,26 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const botUser = await prisma.user.upsert({
-    where: { email: "bot@example.com" },
-    update: {},
-    create: {
-      id: "0",
-      email: "bot@grammarguardian.ar",
-      name: "Grammar Guardian.ar",
-      isGuardian: true,
-    },
+  const existingUser = await prisma.user.findUnique({
+    where: { id: "0" },
   });
 
-  console.log({ botUser });
+  if (!existingUser) {
+    const botUser = await prisma.user.upsert({
+      where: { email: "bot@example.com" },
+      update: {},
+      create: {
+        id: "0",
+        email: "bot@grammarguardian.ar",
+        name: "Grammar Guardian.ar",
+        isGuardian: true,
+      },
+    });
+
+    console.log({ botUser });
+  } else {
+    console.log("User with ID '0' already exists.");
+  }
 }
 
 main()
